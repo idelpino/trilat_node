a1b370e0fc640ad8e8a6c6b1de8d7f35fdda48cb
Pier Tirindelli <pier.tirindelli@gmail.com>
2015-10-19 11:44:29 +0200

    better example in comment and cmakelist cleaned a little

--- src/trilateration_main.cpp
+++ src/trilateration_main.cpp
@@ -2,7 +2,8 @@
  * USAGE:
  *
  * -- To run the gps receiver simulator
- * rosrun trilateration receiver_sim_node  -r 3 -3 2 -s 15 26 40 -s 1 -32 50 -s - -8 70 -s 24 73 56 -s -99 -66 708
+ * rosrun trilateration receiver_sim_node -r 2 2 2 -s -25 36 140 -s 10 -32 150 -s -60 -8 170 -s 24 55 156 -s 99 16 188
+ * rosrun trilateration receiver_sim_node -r 2 2 2 -s -2500 3600 14000 -s 1000 -3200 15000 -s -6000 -800 17000 -s 2400 5500 15600 -s 9900 1600 18800
  *
  * -- To run the trilateration node
  * rosrun trilateration trilateration_node
